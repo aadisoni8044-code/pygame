e89e4723ae50d7b8e203be8e13bc7d8e6f0c067e
aadisoni8044-code <aadisoni8044@gmail.com>
2025-12-20 14:09:33 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+
+for lk in range(5):#          0        
+    print(lk)      #          1 
+                   #          2
+                   #          3
+                   #          4
+    
+    
+for i in range(4):   #   1
+    print('1'*(i+1)) #   11
+                     #   111  
+                     #   1111
+    
+
+
+
+    
